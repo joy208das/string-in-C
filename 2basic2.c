4fef02967d3c56341e9f3992498e88029e6e3736
joy208das <158313870+joy208das@users.noreply.github.com>
2024-05-04 12:26:20 +0600

    Update 2basic2.c

--- 2basic2.c
+++ 2basic2.c
@@ -2,17 +2,17 @@
 
 #include<stdio.h>
 int main(){
-   
-char ch[30];
-gets(ch);
 
-//scanf("%s",&ch);
+char ch[30];
+
+
+scanf("%[^\n]",&ch);  //%[^\n] take input untill newline
 
 printf("%s\n",ch);
 
 
     return 0;
-} 
+}
 
 /*
 akane scanf use kore 
@@ -20,6 +20,9 @@ jodi input dei JOY DAS
 ouput dekbae JOY.
 strinf ar ketre white space ar for character read hoy na.
 tai ja use korte hoy ta holo
+ 
+ %[^\n] take input untill newline
 
+ or
 gets(name_of the variable);
 */
